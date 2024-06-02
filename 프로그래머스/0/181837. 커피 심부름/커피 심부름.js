24860dab81bf65c782b60cdaf53a42e6523e6dda
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2024-06-03 01:14:04 +0900

    [level 0] Title: 커피 심부름, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,10 +1,4 @@
 function solution(order) {
-    let result=0
-    for(item of order){
-        if(item.includes("americano")||item=="anything")
-            result+=4500;
-        else if(item.includes("cafelatte"))
-            result+=5000;
-    }
-    return result;
+    var answer = 0;
+    return answer;
 }
