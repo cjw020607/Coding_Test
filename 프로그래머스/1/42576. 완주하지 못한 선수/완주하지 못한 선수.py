afb467ce8e62d99ee24db279b067a3b478c308e8
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2024-07-12 20:49:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.55 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,22 @@
+from collections import Counter
+def solution(participant, completion):
+    a=Counter(participant)
+    b=Counter(completion)
+    for p in participant:
+        if a[p]!=b[p]:
+            return p
+
+
+
+# def solution(participant, completion):
+#     a=list(set(participant)-set(completion))
+#     if len(a)!=0:
+#         return a[0]
+#     return Counter(participant).most_common(1)[0][0]
+
+# def solution(participant,completion):
+#     l=len(completion)
+#     for i in range(l):
+#         participant.remove(completion[i])
+
+#     return participant[0]
