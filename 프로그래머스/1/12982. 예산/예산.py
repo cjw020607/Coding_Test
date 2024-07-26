f78c64ff855ad8b736c9b2b173b5ae956766f8db
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2024-07-26 16:00:22 +0900

    [level 1] Title: 예산, Time: 0.08 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,9 +1,7 @@
 from itertools import combinations
 def solution(d, budget):
     d.sort()
-    l=len(d)
-    print(d)
-    i=l
+    i=len(d)
     while sum(d[:i])>budget:
         i-=1
     return i
