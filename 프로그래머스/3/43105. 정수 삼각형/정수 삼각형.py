736ec3d8b3f98eb0abcb7cb29d0a051df4294d33
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2024-10-01 09:43:43 +0900

    [level 3] Title: 정수 삼각형, Time: 36.92 ms, Memory: 14.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,9 @@
+def solution(triangle):
+    #bottom->up
+    h=len(triangle)
+    w=len(triangle[h-1])
+    for i in range(h-1,0,-1):
+        for j in range(0,i):
+            triangle[i-1][j]+=max(triangle[i][j],triangle[i][j+1])
+    answer = 0
+    return triangle[0][0]
