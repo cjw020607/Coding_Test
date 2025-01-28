35df6df241266dfd1b4e764365598e11ec3ca72e
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2025-01-29 01:25:52 +0900

    [Silver II] Title: N과 M (10), Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.js
@@ -0,0 +1,23 @@
+fs=require('fs');
+input=fs.readFileSync(0).toString().split('\n');
+let [N,M]=input[0].split(' ').map(Number);
+let arr=input[1].split(' ').map(Number).sort((a,b)=>a-b);
+let result=[];
+let way=[];
+
+const bt=(index)=>{
+    for(let i=index;i<=N;i++){
+        if(way.length===M){
+            result.push(way.join(' '));
+            return;
+        }
+        way.push(arr[i]);
+        bt(i+1);
+        way.pop();
+    }
+}
+
+bt(0);
+
+result=new Set(result);
+console.log([...result].join('\n'));
