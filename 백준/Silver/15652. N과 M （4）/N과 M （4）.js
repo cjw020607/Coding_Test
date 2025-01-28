4ff2c0c1cbfe6706f3c81058d4d3f9325386adf0
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2025-01-28 22:28:25 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.js
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -1,19 +1,21 @@
 fs=require('fs');
-input=fs.readFileSync(0).toString().split('\n');
-let [N,M]=input[0].split(' ').map(Number);
-let way=[]
-const bt=(index)=>{
-    for(let i=index-1; i<N; i++){
-        if(way.length===M){
-            console.log(way.join(' '));
-            return;
-        }
-        else{
-            way.push(i+1);
-            bt(i+1);
-            way.pop(); 
-        }
-    }
+let [N,M]=fs.readFileSync(0).toString().split(' ').map(Number);
+let result=[];
+let way=[];
+
+const bt=(depth)=>{
+ for(let i=depth;i<N;i++){
+  if(way.length===M){
+   result.push(way.join(' '));
+   return;
+  }
+  way.push(i+1);
+  bt(i);
+  way.pop();
+ }
 }
 
-bt(1);
+bt(0);
+console.log(result.join('\n'));
+
+
