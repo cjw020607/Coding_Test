24e5bdfe038ee2f778d2eb51f4fe2d2340374e6f
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2025-02-05 22:22:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.90 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,34 +1,8 @@
-// function solution(participant, completion) {
-//     participant.splice(1,2)
-//     return participant
-//     for (let i = 0; i < completion.length; i++) {
-//         if (participant.includes(completion[i])) {
-//             let index = participant.indexOf(completion[i]);
-//             participant.splice(index, 1);
-//         }
-//     }
-//     return participant[0];
-// }
 function solution(participant, completion) {
-    const map = new Map();
-
-    // participant 배열의 각 요소를 Map에 추가
-    participant.forEach(name => {
-        map.set(name, (map.get(name) || 0) + 1);
-    });
-
-    // completion 배열의 각 요소를 Map에서 제거
-    completion.forEach(name => {
-        if (map.has(name)) {
-            const count = map.get(name);
-            if (count === 1) {
-                map.delete(name);
-            } else {
-                map.set(name, count - 1);
-            }
-        }
-    });
-
-    // Map에 남아있는 유일한 요소 반환
-    return map.keys().next().value;
+   let dic={};
+    participant.forEach(item => dic[item] = (dic[item] || 0) + 1);
+    for(item of completion){
+           dic[item]-=1;
+    }
+    return Object.keys(dic).find(key=>dic[key]>0)
 }
