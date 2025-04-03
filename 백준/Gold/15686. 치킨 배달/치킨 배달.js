e3344321ad794add713421fa7eed1b6d7f4e6218
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2025-04-03 21:24:24 +0900

    [Gold V] Title: 치킨 배달, Time: 200 ms, Memory: 12748 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.js
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -3,6 +3,8 @@ input=fs.readFileSync(0).toString().trim().split('\n');
 let [N,M]=input[0].split(' ').map(Number);
 let arr=[];
 let chicken=[];
+let visited=new Array(chicken.length).fill(0);
+
 for(let i=1;i<=N;i++){
     let a=input[i].split(' ').map(Number);
     for(let j=1;j<=N;j++){
@@ -13,42 +15,42 @@ for(let i=1;i<=N;i++){
     arr.push(a);
 }
 
-const SelectChicken=(chicken_arr, num)=>{
-    let result=[];
-    if(num===1) return chicken_arr.map(item=>[item]);
-    for(let i=0;i<chicken_arr.length;i++){
-        const rest=chicken_arr.slice(i+1)//현재 인덱스 이후의 요소들
-        const a=SelectChicken(rest,num-1);
-        const attached=a.map(item=>[chicken_arr[i],...item]);
-        result.push(...attached)
-    }
-    return result;
-}
-
-let chickenComb=SelectChicken(chicken,M);
-
 let result=Infinity;
+let answer=[];
 
-for(let c=0;c<chickenComb.length;c++){
-    let sum=0;
-
-    for(let i=0;i<N;i++){
-        for(let j=0;j<N;j++){
-            let min=Infinity;
-            //집의 좌표
-            let hy=i+1;
-            let hx=j+1;
-            if(arr[i][j]!==1) continue;
-            for(let [cy,cx] of chickenComb[c]){
-                let dist=Math.abs(hy-cy)+Math.abs(hx-cx);
-                if(dist<min) min=dist;
-                
+const bt=(depth,idx)=>{
+    if(depth===M){
+        let sum=0;
+        for(let i=0;i<N;i++){
+            for(let j=0;j<N;j++){
+                let min=Infinity;
+                //집의 좌표
+                let hy=i+1;
+                let hx=j+1;
+                if(arr[i][j]!==1) continue;
+                //집과 치킨집 사이의 거리
+                for(let [cy,cx] of answer){
+                    let dist=Math.abs(hy-cy)+Math.abs(hx-cx);
+                    if(dist<min) min=dist;
+                    
+                }
+                sum+=min;
             }
-            sum+=min;
+        }
+        result=Math.min(sum,result);
+        return;
+    }
+    for(let i=idx;i<chicken.length;i++){
+        if(!visited[i]){
+            visited[i]=1;
+            answer.push(chicken[i]);
+            bt(depth+1,i);
+            answer.pop();
+            visited[i]=0;
         }
     }
-    result=Math.min(sum,result);
 }
 
 
+bt(0,0);
 console.log(result);
